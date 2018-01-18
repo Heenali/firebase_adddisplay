38e8cd64cf428049e85e5268bc9f48d92f2b989c
Heenali Lakahni <heenali.lakhani@trukker.ae>
2018-01-18 17:59:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/heenali/fire/Ads.java
@@ -0,0 +1,58 @@
+package com.example.heenali.fire;
+
+/**
+ * Created by AndroidJson.com on 5/5/2017.
+ */
+
+public class Ads {
+
+    private String Aug15_Banner_Id;
+    private String Aug15_InterstitialAds_Id;
+    private String Lordshiv_Banner_Id;
+    private String Lordshiv_Interstitial_Id;
+
+    public Ads() {
+        // This is default constructor.
+    }
+
+    public String getAug15_Banner_Id() {
+
+        return Aug15_Banner_Id;
+    }
+
+    public void setAug15_Banner_Id(String Aug15_Banner_Id) {
+
+        this.Aug15_Banner_Id = Aug15_Banner_Id;
+    }
+
+    public String getLordshiv_Banner_Id() {
+
+        return Lordshiv_Banner_Id;
+    }
+
+    public void setLordshiv_Banner_Id(String Lordshiv_Banner_Id) {
+
+        this.Lordshiv_Banner_Id = Lordshiv_Banner_Id;
+    }
+
+
+    public String getAug15_InterstitialAds_Id() {
+        return Aug15_InterstitialAds_Id;
+    }
+
+    public void setAug15_InterstitialAds_Id(String Aug15_InterstitialAds_Id) {
+
+        this.Aug15_InterstitialAds_Id = Aug15_InterstitialAds_Id;
+    }
+
+
+    public String getLordshiv_Interstitial_Id() {
+        return Lordshiv_Interstitial_Id;
+    }
+
+    public void setLordshiv_Interstitial_Id(String Lordshiv_Interstitial_Id) {
+
+        this.Lordshiv_Interstitial_Id = Lordshiv_Interstitial_Id;
+    }
+
+}
